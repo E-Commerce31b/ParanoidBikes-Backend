15467a213bb3b940c1730042deda514fbb90392f
Rodrigo Santesteban <104109457+rodrisantes@users.noreply.github.com>
2023-01-15 16:10:40 -0300

    Update stripe.js

--- api/routes/stripe.js
+++ api/routes/stripe.js
@@ -2,22 +2,23 @@ require("dotenv").config();
 const express = require("express");
 const router = express();
 const Stripe = require("stripe");
+const sendMail = require("../controllers/nodemailer")
 
 const { API_STRIPE } = process.env;
 
 const stripe = new Stripe(API_STRIPE);
 
 router.post("/checkout", async (req, res) => {
-  const { id, amount } = req.body;
+  const { id, amount, email } = req.body;
   try {
     const payment = await stripe.paymentIntents.create({
-      amount,
+      amount : amount,
       currency: "USD",
       description: "Bicicle",
       payment_method: id,
       confirm: true,
     });
-
+    sendMail.sendMail(email)
     res.send({ message: "Pago exitoso" });
     console.log(id)
   } catch (error) {
